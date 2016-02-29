c0058b1d301d9e4cebba753ec837ae60eccee054
albert <junyuo@gmail.com>
2016-02-29 19:55:09 +0800

    add comments for FileTest

--- src/albert/practice/file/FileTest.java
+++ src/albert/practice/file/FileTest.java
@@ -15,6 +15,7 @@ import org.apache.tika.mime.MediaType;
  */
 public class FileTest {
 
+	// set up the path and name of test file
 	private final static String DROPBOX_HOME = "/Users/albert/Dropbox/";
 	private final static String DOC_FILE = DROPBOX_HOME + "庫務組/交付文件(第二階段)/PDS/NTA_PSR_FMS_PDS_1030930_V1.0.doc";
 	private final static String DOCX_FILE = DROPBOX_HOME + "債務管理系統/NTA_IFMIS_DBM_測試及操作文件/DBM001E.docx";
@@ -25,6 +26,7 @@ public class FileTest {
 	private final static String PDF_FILE = DROPBOX_HOME + "eBooks/Head First Python.pdf";
 	private final static String FAKE_EXE_FILE = DROPBOX_HOME + "eBooks/The Intelligent Investor 拷貝.exe";
 
+	// set up the media type list for testing
 	private final static MediaType doc = MediaType.parse("application/msword");
 	private final static MediaType docx = MediaType
 			.parse("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
@@ -39,18 +41,26 @@ public class FileTest {
 		new FileTest().checkFileMediaType(DOC_FILE, doc);
 		new FileTest().checkFileMediaType(DOCX_FILE, docx);
 		new FileTest().checkFileMediaType(XLSX_FILE, xlsx);
+		new FileTest().checkFileMediaType(TXT_FILE, txt);
 		new FileTest().checkFileMediaType(JPG_FILE, jpg);
 		new FileTest().checkFileMediaType(PDF_FILE, pdf);
 		new FileTest().checkFileMediaType(FAKE_EXE_FILE, exe);
 	}
 
 	public void checkFileMediaType(String sourceFile, MediaType expectedMediaType) throws IOException {
+
 		File file = FileUtils.getFile(sourceFile);
+
 		try {
 			Tika tika = new Tika();
 
+			// Detects the media type of the given file. The type detection is
+			// based on the document content and a potential known file
+			// extension.
 			String mediaType = tika.detect(file);
+
 			System.out.println("\nchecking " + sourceFile + "...");
+
 			if (!(expectedMediaType.getBaseType().toString().equals(mediaType))) {
 				String actualMediaTypeName = mediaType;
 				String expectedMediaTypeName = expectedMediaType.getBaseType().toString();
