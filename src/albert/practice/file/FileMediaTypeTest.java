743de84f1f7b73ca6e33404846406c7925a4f231
albert_kuo <albert_kuo@cht.com.tw>
2016-03-02 11:44:14 +0800

    refactor FileMediaTypeTest

--- src/albert/practice/file/FileMediaTypeTest.java
+++ src/albert/practice/file/FileMediaTypeTest.java
@@ -5,76 +5,67 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.tika.Tika;
-import org.apache.tika.mime.MediaType;
+
+import albert.practice.file.enumeration.MediaTypeEnum;
 
 /**
- * MediaType List : http://www.freeformatter.com/mime-types-list.html
  * 
  * @author albert
- *
+ * 
  */
 public class FileMediaTypeTest {
 
-	// set up the path and name of test file
-	private final static String DROPBOX_HOME = "/Users/albert/Dropbox/";
-	private final static String DOC_FILE = DROPBOX_HOME + "庫務組/交付文件(第二階段)/PDS/NTA_PSR_FMS_PDS_1030930_V1.0.doc";
-	private final static String DOCX_FILE = DROPBOX_HOME + "債務管理系統/NTA_IFMIS_DBM_測試及操作文件/DBM001E.docx";
-	private final static String XLSX_FILE = DROPBOX_HOME
-			+ "庫務組/交付文件(第二階段)/PDS/NTA_PSR_FMS_PDS_附件/NTA_PSR_FMS_PDS_1030901_需求追溯表.xlsx";
-	private final static String TXT_FILE = DROPBOX_HOME + "庫務組/測試.txt";
-	private final static String JPG_FILE = DROPBOX_HOME + "庫務組/ads100fa.jpg";
-	private final static String PDF_FILE = DROPBOX_HOME + "eBooks/Head First Python.pdf";
-	private final static String FAKE_EXE_FILE = DROPBOX_HOME + "eBooks/The Intelligent Investor 拷貝.exe";
+    // set up the path and name of test file
+    private final static String DROPBOX_HOME = "D:/dropbox/";
+    private final static String DOC_FILE = DROPBOX_HOME
+            + "庫務組/交付文件(第二階段)/PDS/NTA_PSR_FMS_PDS_1030930_V1.0.doc";
+    private final static String DOCX_FILE = DROPBOX_HOME
+            + "債務管理系統/NTA_IFMIS_DBM_測試及操作文件/DBM001E.docx";
+    private final static String XLSX_FILE = DROPBOX_HOME
+            + "庫務組/交付文件(第二階段)/PDS/NTA_PSR_FMS_PDS_附件/NTA_PSR_FMS_PDS_1030901_需求追溯表.xlsx";
+    private final static String TXT_FILE = DROPBOX_HOME + "庫務組/測試.txt";
+    private final static String JPG_FILE = DROPBOX_HOME + "庫務組/ads100fa.jpg";
+    private final static String PDF_FILE = DROPBOX_HOME + "eBooks/Head First Python.pdf";
+    private final static String FAKE_EXE_FILE = DROPBOX_HOME
+            + "eBooks/The Intelligent Investor 拷貝.exe";
 
-	// set up the media type list for testing
-	private final static MediaType doc = MediaType.parse("application/msword");
-	private final static MediaType docx = MediaType
-			.parse("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-	private final static MediaType xlsx = MediaType
-			.parse("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-	private final static MediaType txt = MediaType.parse("text/plain");
-	private final static MediaType jpg = MediaType.parse("image/jpeg");
-	private final static MediaType pdf = MediaType.parse("application/pdf");
-	private final static MediaType exe = MediaType.parse("application/x-msdownload");
+    public static void main(String[] args) throws IOException {
+        new FileMediaTypeTest().checkFileMediaType(DOC_FILE, MediaTypeEnum.DOC.getMediaType());
+        new FileMediaTypeTest().checkFileMediaType(DOCX_FILE, MediaTypeEnum.DOCX.getMediaType());
+        new FileMediaTypeTest().checkFileMediaType(XLSX_FILE, MediaTypeEnum.XLSX.getMediaType());
+        new FileMediaTypeTest().checkFileMediaType(TXT_FILE, MediaTypeEnum.TXT.getMediaType());
+        new FileMediaTypeTest().checkFileMediaType(JPG_FILE, MediaTypeEnum.JPG.getMediaType());
+        new FileMediaTypeTest().checkFileMediaType(PDF_FILE, MediaTypeEnum.PDF.getMediaType());
+        new FileMediaTypeTest().checkFileMediaType(FAKE_EXE_FILE, MediaTypeEnum.EXE.getMediaType());
+    }
 
-	public static void main(String[] args) throws IOException {
-		new FileMediaTypeTest().checkFileMediaType(DOC_FILE, doc);
-		new FileMediaTypeTest().checkFileMediaType(DOCX_FILE, docx);
-		new FileMediaTypeTest().checkFileMediaType(XLSX_FILE, xlsx);
-		new FileMediaTypeTest().checkFileMediaType(TXT_FILE, txt);
-		new FileMediaTypeTest().checkFileMediaType(JPG_FILE, jpg);
-		new FileMediaTypeTest().checkFileMediaType(PDF_FILE, pdf);
-		new FileMediaTypeTest().checkFileMediaType(FAKE_EXE_FILE, exe);
-	}
+    public void checkFileMediaType(String sourceFile, String expectedMediaType) throws IOException {
 
-	public void checkFileMediaType(String sourceFile, MediaType expectedMediaType) throws IOException {
+        File file = FileUtils.getFile(sourceFile);
 
-		File file = FileUtils.getFile(sourceFile);
+        try {
+            Tika tika = new Tika();
 
-		try {
-			Tika tika = new Tika();
+            // Detects the media type of the given file. The type detection is
+            // based on the document content and a potential known file
+            // extension.
+            String mediaType = tika.detect(file);
 
-			// Detects the media type of the given file. The type detection is
-			// based on the document content and a potential known file
-			// extension.
-			String mediaType = tika.detect(file);
+            System.out.println("\nchecking " + sourceFile + "...");
 
-			System.out.println("\nchecking " + sourceFile + "...");
+            if (!(expectedMediaType.equals(mediaType))) {
+                String actualMediaTypeName = mediaType;
+                String errorMsg = "Worng media type ! Expected:" + expectedMediaType + ", Actual:"
+                        + actualMediaTypeName;
+                System.err.println(errorMsg);
+                throw new RuntimeException(errorMsg);
+            } else {
+                System.out.println("Correct media type : " + mediaType);
+            }
 
-			if (!(expectedMediaType.getBaseType().toString().equals(mediaType))) {
-				String actualMediaTypeName = mediaType;
-				String expectedMediaTypeName = expectedMediaType.getBaseType().toString();
-				String errorMsg = "Worng media type ! Expected:" + expectedMediaTypeName + ", Actual:"
-						+ actualMediaTypeName;
-				System.err.println(errorMsg);
-				throw new RuntimeException(errorMsg);
-			} else {
-				System.out.println("Correct media type : " + mediaType);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/albert/practice/file/enumeration/MediaTypeEnum.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package albert.practice.file.enumeration;
+
+/**
+ * List of MIME types / Internet Media Types: http://www.freeformatter.com/mime-types-list.html
+ */
+public enum MediaTypeEnum {
+
+    DOC("doc", "application/msword"), DOCX("docx",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"), XLSX(
+            "xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"), TXT(
+            "txt", "text/plain"), JPG("jpg", "image/jpeg"), PDF("pdf", "application/pdf"), EXE(
+            "exe", "application/x-msdownload");
+
+    private MediaTypeEnum(String fileExtenstion, String mediaType) {
+        this.fileExtenstion = fileExtenstion;
+        this.mediaType = mediaType;
+    }
+
+    private String fileExtenstion;
+    private String mediaType;
+
+    public String getFileExtenstion() {
+        return fileExtenstion;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+}
