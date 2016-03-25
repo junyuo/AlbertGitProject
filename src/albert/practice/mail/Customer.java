059d0e4afe25cbb8167c6e9ab07929b6ab642254
albert_kuo <albert_kuo@cht.com.tw>
2016-03-25 17:40:38 +0800

    add mail test

---
+++ src/albert/practice/mail/Customer.java
@@ -0,0 +1,66 @@
+package albert.practice.mail;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private String policyNumber;
+    private String name;
+    private String applyDate;
+    private String fromDate;
+    private String toDate;
+    private String place;
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(String applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+}
