ac173830ba2f1b1ab3f0a9b1b281f103b275ba27
albert_kuo <albert_kuo@cht.com.tw>
2016-04-07 14:37:22 +0800

    using Project Lombok

--- src/albert/practice/xstream/XStreamTest2.java
+++ src/albert/practice/xstream/XStreamTest2.java
@@ -1,32 +1,35 @@
 package albert.practice.xstream;
 
-import com.thoughtworks.xstream.XStream;
-
+import lombok.extern.slf4j.Slf4j;
 import albert.practice.xstream.beans.Doc;
 
+import com.thoughtworks.xstream.XStream;
+
+@Slf4j
 public class XStreamTest2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        // prepare bean for XML
+        Doc doc = new Doc();
+        doc.setRegId("123456");
+        doc.setOwnerId("A123456789");
+        doc.setPolicyNumber("TA800000001");
+        doc.setInsuredId("B123456789");
+        doc.setFormId("A00123");
 
-		// prepare bean for XML
-		Doc doc = new Doc();
-		doc.setRegId("123456");
-		doc.setOwnerId("A123456789");
-		doc.setPolicyNumber("TA800000001");
-		doc.setInsuredId("B123456789");
-		doc.setFormId("A00123");
+        log.info("doc = " + doc.toString());
 
-		// Initializing XStream
-		XStream xStream = new XStream();
+        // Initializing XStream
+        XStream xStream = new XStream();
 
-		// create an alias called doc to the desired class (Doc)
-		xStream.alias("doc", Doc.class);
+        // create an alias called doc to the desired class (Doc)
+        xStream.alias("doc", Doc.class);
 
-		// Serializing an object to XML
-		String xml = xStream.toXML(doc);
+        // Serializing an object to XML
+        String xml = xStream.toXML(doc);
 
-		// print XML
-		System.out.println(xml);
-	}
+        // print XML
+        log.debug("xml = " + xml);
+    }
 
 }
