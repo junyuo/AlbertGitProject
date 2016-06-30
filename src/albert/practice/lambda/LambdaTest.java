e1f214c7ab04deef529da693ab9df2fe52aa2ade
albert_kuo <albert_kuo@cht.com.tw>
2016-06-30 15:00:37 +0800

    update LambdaTest

--- src/albert/practice/lambda/LambdaTest.java
+++ src/albert/practice/lambda/LambdaTest.java
@@ -2,6 +2,8 @@ package albert.practice.lambda;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -15,6 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LambdaTest {
+
+    private int player;
 
     public static void main(String[] args) {
 	LambdaTest test = new LambdaTest();
@@ -45,10 +49,15 @@ public class LambdaTest {
 
 	long numOfMale = dummyData.stream().filter(p -> p.getGender().equals(Gender.MALE)).count();
 	log.debug("numOfMale = " + numOfMale);
-	
-	
+
 	test.separatePlayersWithComma();
 	test.separatePlayersWithComma_usingJDK8();
+
+	// test.findPlayer();
+	// test.findPlayer_usingJDK8();
+
+	test.removeDulpicatePlayerAndSort();
+	test.removeDulpicatePlayerAndSort_usingJDK8();
     }
 
     @SuppressWarnings("unchecked")
@@ -118,23 +127,80 @@ public class LambdaTest {
 	return Arrays.asList("王 柏 融", "林 智 勝", "林 泓 育", "蔣 智 賢", "高 國 慶");
     }
 
-    private void separatePlayersWithComma(){
-	List<String> players =getDummyPlayers();
+    private void separatePlayersWithComma() {
+	List<String> players = getDummyPlayers();
 	String playersWithComma = "";
 	for (int i = 0; i < players.size(); i++) {
 	    if (i == players.size() - 1) {
 		playersWithComma = playersWithComma + players.get(i);
 	    } else {
-		playersWithComma = playersWithComma + players.get(i) +", "; 
+		playersWithComma = playersWithComma + players.get(i) + ", ";
 	    }
 	}
 	log.debug("playersWithComma = " + playersWithComma);
     }
-    
-    private void separatePlayersWithComma_usingJDK8(){
-	List<String> players =getDummyPlayers();
+
+    private void separatePlayersWithComma_usingJDK8() {
+	List<String> players = getDummyPlayers();
 	String playersWithComma = players.stream().collect(Collectors.joining(", "));
 	log.debug("playersWithComma = " + playersWithComma);
     }
 
+    // using Apache Commons CollectionUtils
+    private void findPlayer() {
+	List<String> players = getDummyPlayers();
+	String name = "陳 金 鋒";
+	List<String> result = (List<String>) CollectionUtils.select(players, new Predicate() {
+	    @Override
+	    public boolean evaluate(Object object) {
+		String player = (String) object;
+		return name.equals(player);
+	    }
+	});
+
+	if (CollectionUtils.isEmpty(result)) {
+	    throw new RuntimeException("Cannot find player with name " + name);
+	}
+    }
+
+    // Using Lambda expression
+    private void findPlayer_usingJDK8() {
+	List<String> players = getDummyPlayers();
+	String name = "陳 金 鋒";
+	players.stream().filter(player -> name.equals(player)).findFirst()
+		.orElseThrow(() -> new RuntimeException("Cannot find player with name " + name));
+    }
+
+    private List<String> getDummyPlayersWithDuplicateValue() {
+	return Arrays.asList("王 柏 融", "林 智 勝", "林 泓 育", "王 柏 融", "林 智 勝", "蔣 智 賢", "高 國 慶");
+    }
+
+    // using Apache Commons CollectionUtils
+    private void removeDulpicatePlayerAndSort() {
+	List<String> players = getDummyPlayersWithDuplicateValue();
+	List<String> newList = new ArrayList<>();
+
+	for (String player : players) {
+	    if (!newList.contains(player)) {
+		newList.add(player);
+	    }
+	}
+
+	Collections.sort(newList, new Comparator<String>() {
+	    @Override
+	    public int compare(String o1, String o2) {
+		return o1.compareTo(o2);
+	    }
+	});
+
+	newList.stream().forEach(player -> log.debug(player.toString()));
+    }
+
+    // Using Lambda expression
+    private void removeDulpicatePlayerAndSort_usingJDK8() {
+	List<String> players = getDummyPlayersWithDuplicateValue();
+	List<String> newList = players.stream().map(player -> player).distinct().sorted((o1, o2) -> o1.compareTo(o2))
+		.collect(Collectors.toList());
+	newList.stream().forEach(player -> log.debug(player.toString()));
+    }
 }
